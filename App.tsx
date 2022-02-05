8c9c75908fa7a238d86f1efc16d91fbed4c302e1
Luiz Rioja <lulyrioja@gmail.com>
2022-02-05 00:30:54 -0300

    chore(App): loads the application's fonts and theme

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View } from "react-native";
+import AppLoading from "expo-app-loading";
+import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+import { ThemeProvider } from "styled-components/native";
+import theme from "@src/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <View></View>
+    </ThemeProvider>
+  );
+}
