d952a7b6f5ca4d0a3341f667b7d3f3737231836c
Luiz Rioja <lulyrioja@gmail.com>
2022-02-04 23:57:52 -0300

    chore: configure moduleResolver in ts and babelConfig

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: [
+      "babel-preset-expo",
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".js", ".json"],
+          alias: {
+            "@assets": "./src/assets",
+            "@screens": "./src/screens",
+            "@components": "./src/components",
+          },
+        },
+      ],
+    ],
   };
 };
