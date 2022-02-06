663138856bb83fd19a7b49263d31e086b70774eb
Luiz Rioja <lulyrioja@gmail.com>
2022-02-05 23:42:32 -0300

    fix(babelConfig): insert the plugins key and declare the src alias

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      "babel-preset-expo",
+    presets: ["babel-preset-expo"],
+    plugins: [
       [
         "module-resolver",
         {
           root: ["./src"],
           extensions: [".ts", ".tsx", ".js", ".json"],
           alias: {
+            "@src": "./src",
             "@assets": "./src/assets",
             "@screens": "./src/screens",
             "@components": "./src/components",
