b23bf4841ca637ab5d1844a394e4d4e9d17c63da
Luiz Rioja <lulyrioja@gmail.com>
2022-02-07 23:48:50 -0300

    refac(App): remove unused import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { View } from "react-native";
 import AppLoading from "expo-app-loading";
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
