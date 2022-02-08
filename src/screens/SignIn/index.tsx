8bd3119994f642196699e4b7036e541124b1c769
Luiz Rioja <lulyrioja@gmail.com>
2022-02-07 23:50:39 -0300

    chore: add inputs to the signIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import { Input } from "@src/components/Input";
 import { Container } from "./styles";
 
 export function SignIn() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Input
+        placeholder="E-mail"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize={"none"}
+      />
+
+      <Input placeholder="Senha" type="secondary" secureTextEntry />
+    </Container>
+  );
 }
