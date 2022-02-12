26d5eb1e6a5923a74820e4ee8482b6b6915d2e7c
Luiz Rioja <lulyrioja@gmail.com>
2022-02-11 23:29:50 -0300

    chore(SignIn): finish implementation of signIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,22 +1,44 @@
 import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 import { Input } from "@src/components/Input";
-import { Container } from "./styles";
 import { Button } from "@src/components/Button";
+import {
+  Brand,
+  Container,
+  Content,
+  ForgotPasswordButton,
+  ForgotPasswordLabel,
+  Title,
+} from "./styles";
+
+import brandImg from "@assets/brand.png";
 
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize={"none"}
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize={"none"}
+          />
 
-      <Input placeholder="Senha" type="secondary" secureTextEntry />
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
 
-      <Button title="Entrar" type="secondary" />
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button title="Entrar" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
