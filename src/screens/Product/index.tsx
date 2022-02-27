d9132d4ae7e5b85f58a5aa6cc4817e1c2d7ccf15
Luiz Rioja <lulyrioja@gmail.com>
2022-02-26 23:55:56 -0300

    chore(Product): add the components in the products screen

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Platform } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-import { Container } from "./styles";
+import { Container, Header, Title, DeleteLabel } from "./styles";
 
 export function Product() {
   return (
-    <Container
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
-    ></Container>
+    <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
+      <Header>
+        <Title>Cadastrar</Title>
+
+        <TouchableOpacity>
+          <DeleteLabel>Deletar</DeleteLabel>
+        </TouchableOpacity>
+      </Header>
+    </Container>
   );
 }
