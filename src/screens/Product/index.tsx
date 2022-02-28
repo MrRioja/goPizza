1fd5b291bdcf99b6106e96965de8772511998ee3
Luiz Rioja <lulyrioja@gmail.com>
2022-02-27 23:47:49 -0300

    chore(Product): adds the backButton to the product web

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Platform } from "react-native";
+import { BackButton } from "@src/components/BackButton";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 import { Container, Header, Title, DeleteLabel } from "./styles";
@@ -8,6 +9,7 @@ export function Product() {
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <Header>
+        <BackButton />
         <Title>Cadastrar</Title>
 
         <TouchableOpacity>
