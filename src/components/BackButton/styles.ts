852b7f408462cf1e937598b207a6e840121dfeff
Luiz Rioja <lulyrioja@gmail.com>
2022-03-01 18:12:47 -0300

    fix(BackButton): add units of measure missing in styling

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components/native";
 
 export const Container = styled.TouchableOpacity`
-  width: 40;
-  height: 40;
+  width: 40px;
+  height: 40px;
   border-radius: 12px;
   align-items: center;
   justify-content: center;
