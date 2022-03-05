28069970057d929ad5a6d4b13192e8467e6dae8b
Luiz Rioja <lulyrioja@gmail.com>
2022-03-05 18:31:42 -0300

    chore(Home): adds new components on the screen and fetches data from pizzas in firestore

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "styled-components";
 import { MaterialIcons } from "@expo/vector-icons";
-import { TouchableOpacity } from "react-native";
+import { Alert, FlatList, TouchableOpacity } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 
 import happyEmoji from "@assets/happy.png";
+import { Search } from "@src/components/Search";
 
 import {
   Container,
   Header,
   Greeting,
+  Title,
   GreetingEmoji,
   GreetingText,
+  MenuHeader,
+  MenuItemsNumber,
 } from "./styles";
+import { ProductCard, ProductProps } from "@src/components/ProductCard";
 
 export function Home() {
+  const [pizzas, setPizzas] = useState<ProductProps[]>([]);
+  const [search, setSearch] = useState("");
   const { COLORS } = useTheme();
+
+  async function fetchPizzas(value: string) {
+    const formattedValue = value.toLocaleLowerCase().trim();
+
+    firestore()
+      .collection("pizzas")
+      .orderBy("name_insensitive")
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then((response) => {
+        const data = response.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+
+        setPizzas(data);
+      })
+      .catch(() =>
+        Alert.alert("Consulta", "Não foi possível realizar a consulta")
+      );
+  }
+
+  async function handleSearch() {
+    fetchPizzas(search);
+  }
+
+  async function handleSearchClear() {
+    setSearch("");
+    fetchPizzas("");
+  }
+
+  useEffect(() => {
+    fetchPizzas("");
+  }, []);
 
   return (
     <Container>
@@ -28,6 +73,30 @@ export function Home() {
           <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
         </TouchableOpacity>
       </Header>
+
+      <Search
+        onChangeText={setSearch}
+        value={search}
+        onClear={handleSearchClear}
+        onSearch={handleSearch}
+      />
+
+      <MenuHeader>
+        <Title>Cardápio</Title>
+        <MenuItemsNumber>10 pizzas</MenuItemsNumber>
+      </MenuHeader>
+
+      <FlatList
+        data={pizzas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <ProductCard data={item} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingTop: 20,
+          paddingBottom: 125,
+          marginHorizontal: 24,
+        }}
+      />
     </Container>
   );
 }
