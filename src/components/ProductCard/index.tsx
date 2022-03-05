8f1b448e2b6aec002f95a7e633b82a313d6017e2
Luiz Rioja <lulyrioja@gmail.com>
2022-03-05 18:30:46 -0300

    chore(ProductCard): create and style the component

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { RectButtonProps } from "react-native-gesture-handler";
+import { useTheme } from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
+
+import {
+  Container,
+  Content,
+  Description,
+  Details,
+  Image,
+  Line,
+  Name,
+  Identification,
+} from "./styles";
+
+export type ProductProps = {
+  id: string;
+  photoUrl: string;
+  name: string;
+  description: string;
+};
+
+type Props = RectButtonProps & {
+  data: ProductProps;
+};
+
+export function ProductCard({ data, ...rest }: Props) {
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+      <Content {...rest}>
+        <Image source={{ uri: data.photoUrl }} />
+
+        <Details>
+          <Identification>
+            <Name>{data.name}</Name>
+            <Feather name="chevron-right" size={18} color={COLORS.SHAPE} />
+          </Identification>
+
+          <Description>{data.description}</Description>
+        </Details>
+      </Content>
+
+      <Line />
+    </Container>
+  );
+}
