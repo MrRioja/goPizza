cc46cf2350bc5cc300cdb1bd3d648def99089896
Luiz Rioja <lulyrioja@gmail.com>
2022-03-06 17:22:41 -0300

    chore(routes): makes application routes dynamic based on login and user type

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
+import { useAuth } from "@src/hooks/auth";
+import { SignIn } from "@src/screens/SignIn";
 import { UserStackRoutes } from "./user.stack.routes";
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
     <NavigationContainer>
-      <UserStackRoutes />
+      {user ? <UserStackRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }
--- src/routes/user.stack.routes.tsx
+++ src/routes/user.stack.routes.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useAuth } from "@src/hooks/auth";
 
 import { Home } from "@src/screens/Home";
 import { Product } from "@src/screens/Product";
+import { Order } from "@src/screens/Order";
+import { UserTabRoutes } from "./user.tab.routes";
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen, Group } = createNativeStackNavigator();
 
 export function UserStackRoutes() {
+  const { user } = useAuth();
+
   return (
     <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="home" component={Home} />
-      <Screen name="product" component={Product} />
+      {user?.isAdmin ? (
+        <Group>
+          <Screen name="home" component={Home} />
+          <Screen name="product" component={Product} />
+        </Group>
+      ) : (
+        <Group>
+          <Screen name="userTabRoutes" component={UserTabRoutes} />
+          <Screen name="order" component={Order} />
+        </Group>
+      )}
     </Navigator>
   );
 }
