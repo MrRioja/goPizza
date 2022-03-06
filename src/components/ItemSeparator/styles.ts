f6f209220584dd4a6bda8905200b3d9894cc25a3
Luiz Rioja <lulyrioja@gmail.com>
2022-03-06 17:16:39 -0300

    chore(ItemSeparator): implement and style the component

---
+++ src/components/ItemSeparator/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const Separator = styled.View`
+  width: 100%;
+  height: 1px;
+  background-color: ${({ theme }) => theme.COLORS.SHAPE};
+`;
