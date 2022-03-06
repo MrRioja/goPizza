93a9fc2a5d834a7bbe5c707e0e255fa4e5e42700
Luiz Rioja <lulyrioja@gmail.com>
2022-03-06 17:16:18 -0300

    chore(BottomMenu): implement and style the component

---
+++ src/components/BottomMenu/styles.ts
@@ -0,0 +1,54 @@
+import styled, { css } from "styled-components/native";
+
+type TitleProps = {
+  color: string;
+};
+
+type NotificationProps = {
+  noNotifications: boolean;
+};
+
+export const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Title = styled.Text<TitleProps>`
+  font-size: 18px;
+
+  ${({ theme, color }) => css`
+    font-family: ${theme.FONTS.TITLE};
+    color: ${color};
+  `}
+`;
+
+export const Notification = styled.View<NotificationProps>`
+  height: 20px;
+  border-radius: 12px;
+  align-items: center;
+  justify-content: center;
+  padding: 0 12px;
+  margin-left: 8px;
+
+  ${({ theme, noNotifications }) =>
+    !noNotifications &&
+    css`
+      background-color: ${theme.COLORS.SUCCESS_900};
+    `}
+
+  ${({ theme, noNotifications }) =>
+    noNotifications &&
+    css`
+      background-color: transparent;
+      border: 1px solid ${theme.COLORS.SHAPE};
+    `}
+`;
+
+export const Quantity = styled.Text<NotificationProps>`
+  font-size: 12px;
+
+  ${({ theme, noNotifications }) => css`
+    font-family: ${theme.FONTS.TEXT};
+    color: ${noNotifications ? theme.COLORS.SECONDARY_500 : theme.COLORS.TITLE};
+  `}
+`;
